efa3244f75e3ea45cea4946527a99e219447c66c
Christopher Morgan <sirhcnagrom@outlook.com>
2019-10-16 22:45:29 -0500

    Added X and O

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -4,9 +4,31 @@ window.onload = function(){
     var board = document.getElementById("board");
     var squares = board.getElementsByTagName("div");
 
+
     for (var i  = 0; i < squares.length; i+=1){
+        var count = 1; 
         squares[i].classList.add("square");
+        squares[i].addEventListener("click", function() {
+            if(count % 2 === 1){
+                if (this.innerHTML != "X" && this.innerHTML != "O"){
+                    this.classList.add("X");
+                    this.innerHTML = "X";
+                    
+                }
+            }
+            else{
+                if (this.innerHTML != "X" && this.innerHTML != "O"){
+                    this.classList.add("O");
+                    this.innerHTML = "O";
+
+            }
+        }
+            count+=1;
+
+        })
+        
     }
+    
 };
 
 
